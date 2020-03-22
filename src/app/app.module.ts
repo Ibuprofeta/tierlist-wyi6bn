0dd4864ba81ccaee89e3467308def4b715f3b8f0
Ibuprofeta <edu.blue14@gmail.com>
2020-03-22 13:30:24 +0100

    2 files modified, 7 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
 
-import { TierListComponent } from './champion-tier-list/champion-tier-list.component'
-
-import { ChampionsComponent } from './champions/champions.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule,  AppRoutingModule],
-  declarations: [ AppComponent, TierListComponent, ChampionsComponent],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
