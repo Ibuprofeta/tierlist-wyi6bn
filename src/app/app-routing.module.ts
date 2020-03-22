e5a32800df0cd8b1be0b6beaa22ae0ae3d66dcb5
Ibuprofeta <edu.blue14@gmail.com>
2020-03-22 13:17:08 +0100

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TierListComponent } from './champion-tier-list/champion-tier-list.comp'
+import { TierListComponent } from './champion-tier-list/champion-tier-list.component'
+
+import { ChampionsComponent } from './champions/champions.component'
 
 // per cada ruta definim el path i el component que la gestionarà
 
 const routes: Routes = [
   {
     path: '',
+    component: ChampionsComponent
   },
   {
-    path: 'about/:parametre',
+    path: 'tierlist',
+    component: TierListComponent
   },
 ];
 
