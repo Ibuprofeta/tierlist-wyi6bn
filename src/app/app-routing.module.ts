971d42546bcb40595b1afc4c912415436088ac1d
Ibuprofeta <edu.blue14@gmail.com>
2020-03-22 13:21:07 +0100

    3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TierListComponent } from './champion-tier-list/champion-tier-list.component'
+import { TierListComponent } from './champion-tier-list/champion-tier-list.component';
 
-import { ChampionsComponent } from './champions/champions.component'
+import { ChampionsComponent } from './champions/champions.component';
 
 // per cada ruta definim el path i el component que la gestionarà
 
