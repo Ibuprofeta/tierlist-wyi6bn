369c7669780c5d483d5ec87234649c9d408e5282
Ibuprofeta <edu.blue14@gmail.com>
2020-04-14 18:13:15 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TierlistComponent } from './tierlist/tierlist.component';
 // firebase 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
   imports:      [ 
